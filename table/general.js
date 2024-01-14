5f1f0a74baf623582eedfb33e715cebeca33b49e
Daniel <adonchev111@gmail.com>
2024-01-14 20:31:29 +0200

    adding js and hide show functionality for the dialog box

---
+++ table/general.js
@@ -0,0 +1,18 @@
+function showDialogBox() {
+    $(".invisible").css("display", "block");
+}
+
+function add()
+{
+    hideDialogBox();
+}
+
+
+function cancel()
+{
+    hideDialogBox();
+}
+
+function hideDialogBox() {
+    $(".invisible").css("display", "none")
+}
