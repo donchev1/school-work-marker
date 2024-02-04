e25613d4be6d423fb129241d1ad32ab3dd1c9a9b
Daniel <adonchev111@gmail.com>
2024-02-04 20:55:53 +0200

    adding jquery

--- table/general.js
+++ table/general.js
@@ -1,9 +1,34 @@
-function showDialogBox() {
+function showDialogBox(day,hour) {
     $(".invisible").css("display", "block");
+    var dayDiv = $(".current-day");
+    dayDiv.text(day);
+    var hourDiv = $(".current-hour");
+    hourDiv.text(hour);
+    debugger;
+    var domashnoZaToziChas = $("#" + hour+ "-" + day).text();
+    $("#homeworkInput")[0].value = domashnoZaToziChas; 
+
+    
 }
 
 function add()
 {
+    var domashno = $("#homeworkInput")[0].value; 
+    var currentHour = $(".current-hour")[0].innerText;
+    var currentday = $(".current-day")[0].innerText;
+    var existingData = $("#" + currentHour+ "-" + currentday);
+    if (existingData[0] == undefined)
+    {
+        var newDivWithData = $("<div id='" + currentHour + "-" + currentday +"'> </div>").text(domashno);
+        $(".data-div").append(newDivWithData);
+    }
+    else 
+    {
+        
+        existingData.text(domashno);
+    }
+
+    
     hideDialogBox();
 }
 
@@ -14,5 +39,6 @@ function cancel()
 }
 
 function hideDialogBox() {
+    $("#homeworkInput")[0].value = "";
     $(".invisible").css("display", "none")
 }
